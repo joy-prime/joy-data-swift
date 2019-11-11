7e0b6f28acdc27ee938d93c8c51a4063878e9fe2
deansher <deansherthompson@gmail.com>
2019-11-11 07:23:04 -0500

    Created an xcode project.

---
+++ MapKey.swift
@@ -0,0 +1,17 @@
+//
+//  MapKey.swift
+//  joy-data-swift
+//
+//  Created by Dean Thompson on 11/11/19.
+//  Copyright © 2019 felicity-todo. All rights reserved.
+//
+
+import Foundation
+
+public class MapKeys {
+    let namespace: String
+    
+    init(namespace: String) {
+        self.namespace = namespace
+    }
+}
