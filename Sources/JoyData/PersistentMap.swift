657ffb5db1a0e22b1511529e1b72a5528004d3d3
deansher <deansherthompson@gmail.com>
2019-11-24 12:18:21 -0500

    Implemented PersistentMap

---
+++ Sources/JoyData/PersistentMap.swift
@@ -0,0 +1,9 @@
+//
+//  PersistentDict.swift
+//  
+//
+//  Created by Dean Thompson on 11/16/19.
+//
+
+import Foundation
+
---
+++ Sources/JoyDataTests/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Dean Thompson on 11/24/19.
+//
+
+import Foundation
